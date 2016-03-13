6d675da4563b463dca0705e73a393814e94fe713
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-03-13 22:00:00 +0000

    Add file Receipt.rb to lib directory

---
+++ lib/Receipt.rb
@@ -0,0 +1,6 @@
+# Receipt class; each instance is a trip, and stores both overall expenditure, as well as
+# every receipt, with information on who paid, what for, who for, etc
+
+class Receipt
+
+end
